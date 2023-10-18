0ecd6e5bffee89e851c945a1eae8c0a6eb3a224b
Admin <admin.push@hackerrank.workspace>
2023-10-18 14:26:26 +0000

    Auto save changes before Run Tests
    Commit by HackerRank

--- src/main/java/com/inditex/zarachallenge/controller/SimilarController.java
+++ src/main/java/com/inditex/zarachallenge/controller/SimilarController.java
@@ -1,6 +1,19 @@
 package com.inditex.zarachallenge.controller;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/product")
 public class SimilarController {
 
-	// TODO: Insert code in this controller
+
+	@GetMapping("/product/{id}/similar")
+	public ResponseEntity<String> getProductSimilarid(@PathVariable Integer id) {
+		System.out.println("id:" + id);
+		return null;
+	}
 }
